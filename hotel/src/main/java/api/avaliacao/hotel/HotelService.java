1fa1109f9f6cd0dd6d31f61ae109a642e806ee40
Isabelleatt <isabelledasilvasantos1@gmail.com>
2023-05-16 11:53:34 -0300

    Adcionando Comentários

--- hotel/src/main/java/api/avaliacao/hotel/HotelService.java
+++ hotel/src/main/java/api/avaliacao/hotel/HotelService.java
@@ -11,6 +11,7 @@ public class HotelService {
     @Autowired
     private HotelRepository hotelRepository;
 
+    // GET /hotel -> lista todos os filmes cadastrados. Esta rota deve ter dois query params, um que filtra os hoteis pelo número de estrelas, de 1 a 5, e outro que filtra pelo valor minímo da diária.
     public List<Hotel> getHotels(Integer estrelas, Double valorMinimoDiaria) {
         if (estrelas != null && valorMinimoDiaria != null) {
             return hotelRepository.findByEstrelasAndValorDiariaGreaterThanEqual(estrelas, valorMinimoDiaria);
@@ -23,19 +24,23 @@ public class HotelService {
         }
     }
 
+    // GET /hotel/{idHotel} -> Recupera um hotel específico.POST /hotel -> Cria um hotel, podem definir os dados que serão salvos, mas no minímo deve ter o número de estrelas e o valor da diária.
     public Hotel getHotel(Long idHotel) {
         return hotelRepository.findById(idHotel)
                 .orElseThrow(() -> new HotelNotFoundException("Hotel não encontrado"));
     }    
 
+    // POST /hotel -> Cria um hotel, podem definir os dados que serão salvos, mas no minímo deve ter o número de estrelas e o valor da diária.
     public Hotel createHotel(Hotel hotel) {
         return hotelRepository.save(hotel);
     }
 
+    // DELETE /hotel -> Deleta um hotel do banco de dados.
     public void deleteHotel(Long idHotel) {
         hotelRepository.deleteById(idHotel);
     }
 
+    // Exceção para quando o hotel não for encontrado
     public class HotelNotFoundException extends RuntimeException {
         public HotelNotFoundException(String message) {
             super(message);
